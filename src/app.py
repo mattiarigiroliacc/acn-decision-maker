cf18bffb7a7299a161e4162dd266900c334b373b
Cesareo <giacomo.cesareo@accenture.com>
2022-06-28 18:07:44 +0200

    init app

---
+++ src/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+     page_title="Ex-stream-ly Cool App",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+ )
+
+st.write("hello")
